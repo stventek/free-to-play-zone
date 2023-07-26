de931a07a1abd1e35f6c64b678a47019794befd9
stventek <Xenizs@protonmail.com>
2023-07-26 16:13:19 -0400

    switch to ngboostrap carousel

--- src/app/pages/game-detail/components/screenshots-carousel/screenshots-carousel.component.ts
+++ src/app/pages/game-detail/components/screenshots-carousel/screenshots-carousel.component.ts
@@ -1,11 +1,34 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { ScreeenshotData } from '../../types/screenshot-data.interface';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-screenshots-carousel',
   templateUrl: './screenshots-carousel.component.html',
   styleUrls: ['./screenshots-carousel.component.scss'],
 })
-export class ScreenshotsCarouselComponent {
+export class ScreenshotsCarouselComponent implements OnChanges, AfterViewInit {
   @Input() screenshots!: ScreeenshotData[];
+
+  @ViewChild('screenshotCarousel', { static: true }) carousel!: NgbCarousel;
+  private carouselInitialized = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.carouselInitialized) {
+      this.carousel.select('active-screenshot');
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.carouselInitialized = true;
+    this.carousel.select('active-screenshot');
+  }
 }
--- src/app/pages/game-detail/game-detail.module.ts
+++ src/app/pages/game-detail/game-detail.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
 import { GameDetailService } from './services/game-detail.service';
 import { ScreenshotsCarouselComponent } from './components/screenshots-carousel/screenshots-carousel.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [GameDetailComponent, ScreenshotsCarouselComponent],
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, NgbCarouselModule],
   exports: [GameDetailComponent],
   providers: [GameDetailService],
 })
